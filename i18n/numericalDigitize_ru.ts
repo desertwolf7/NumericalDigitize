2cb4766feeee669776aa6ee55fccd238dbea200f
desertwolf7 <57559510+desertwolf7@users.noreply.github.com>
2020-02-01 15:00:39 +0700

    Add files via upload

---
+++ i18n/numericalDigitize_ru.ts
@@ -0,0 +1,266 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="ru_RU">
+<context>
+    <name>AddFeatureGui</name>
+    <message>
+        <location filename="../addFeatureGUI.py" line="329"/>
+        <source>Select source coordinates system</source>
+        <translation>Выберите исходную систему координат</translation>
+    </message>
+    <message>
+        <location filename="../addFeatureGUI.py" line="333"/>
+        <source>Select destination coordinates system</source>
+        <translation>Выберите новую систему координат</translation>
+    </message>
+    <message>
+        <location filename="../addFeatureGUI.py" line="556"/>
+        <source>Warning</source>
+        <translation>Предупреждение</translation>
+    </message>
+    <message>
+        <location filename="../addFeatureGUI.py" line="689"/>
+        <source>Values error</source>
+        <translation>Неправильные значения</translation>
+    </message>
+    <message>
+        <location filename="../addFeatureGUI.py" line="689"/>
+        <source>Current part contains incorrect values</source>
+        <translation>Текущая часть содержит некорректные значения</translation>
+    </message>
+    <message>
+        <location filename="../addFeatureGUI.py" line="312"/>
+        <source>CRS not selected</source>
+        <translation>Не выбрана CRS</translation>
+    </message>
+    <message>
+        <location filename="../addFeatureGUI.py" line="556"/>
+        <source>Remove all rows?</source>
+        <translation>Удалить все строки?</translation>
+    </message>
+</context>
+<context>
+    <name>NumericalDigitize</name>
+    <message>
+        <location filename="../numericalDigitize.py" line="186"/>
+        <source>Add feature</source>
+        <translation>Добавить объект</translation>
+    </message>
+    <message>
+        <location filename="../numericalDigitize.py" line="225"/>
+        <source>No feature selected</source>
+        <translation>Не выбрано ни одного объекта</translation>
+    </message>
+    <message>
+        <location filename="../numericalDigitize.py" line="230"/>
+        <source>More then 1 feature selected</source>
+        <translation>Выбрано более одного объекта</translation>
+    </message>
+    <message>
+        <location filename="../numericalDigitize.py" line="251"/>
+        <source>Edit feature</source>
+        <translation>Редактировать объект</translation>
+    </message>
+    <message>
+        <location filename="../numericalDigitize.py" line="475"/>
+        <source>Ring not in exterior contour</source>
+        <translation>Кольцо находится вне внешнего контура полигона</translation>
+    </message>
+    <message>
+        <location filename="../numericalDigitize.py" line="494"/>
+        <source>Feature not valid</source>
+        <translation>Объекта имеет неправильную геометрию</translation>
+    </message>
+    <message>
+        <location filename="../numericalDigitize.py" line="109"/>
+        <source>Numerical digitizing</source>
+        <translation>Добавление объектов по координатам точек</translation>
+    </message>
+    <message>
+        <location filename="../numericalDigitize.py" line="118"/>
+        <source>Numerical edit</source>
+        <translation>Координатное редактирование объекта</translation>
+    </message>
+    <message>
+        <location filename="../numericalDigitize.py" line="106"/>
+        <source>Numerical digitize</source>
+        <translation>Добавление объектов по координатам точек</translation>
+    </message>
+    <message>
+        <location filename="../numericalDigitize.py" line="230"/>
+        <source>Numerical edit error</source>
+        <translation>Ошибка выбора объекта для редактирования</translation>
+    </message>
+    <message>
+        <location filename="../numericalDigitize.py" line="494"/>
+        <source>The new geometry of the feature isn&apos;t valid. Do you want to use it anyway?</source>
+        <translation>Новая геометрия объекта неправильная. Вы хотите использованить ее использовать?</translation>
+    </message>
+    <message>
+        <location filename="../numericalDigitize.py" line="144"/>
+        <source>&amp;Numerical Digitize</source>
+        <translation>&amp;Добавление и редактирование объекта по координатам точек</translation>
+    </message>
+    <message>
+        <location filename="../numericalDigitize.py" line="374"/>
+        <source>Help</source>
+        <translation>Помощь</translation>
+    </message>
+    <message>
+        <location filename="../numericalDigitize.py" line="206"/>
+        <source>/index.html</source>
+        <translation>/index_ru.html</translation>
+    </message>
+</context>
+<context>
+    <name>ValueChecker</name>
+    <message>
+        <location filename="../valueChecker.py" line="145"/>
+        <source>Part </source>
+        <translation>Часть </translation>
+    </message>
+    <message>
+        <location filename="../valueChecker.py" line="122"/>
+        <source> is empty</source>
+        <translation> не содержит значений</translation>
+    </message>
+    <message>
+        <location filename="../valueChecker.py" line="125"/>
+        <source> contains below 2 points. It&apos;s not enough for creating line geometry</source>
+        <translation> содержит менее 2 точек. Этого недостаточно для создания линии</translation>
+    </message>
+    <message>
+        <location filename="../valueChecker.py" line="130"/>
+        <source> contains below 3 points. It&apos;s not enough for creating polygon geometry</source>
+        <translation> содержит менее 3 точек. Этого недостаточно для создания полигона</translation>
+    </message>
+    <message>
+        <location filename="../valueChecker.py" line="145"/>
+        <source> row </source>
+        <translation> строка </translation>
+    </message>
+    <message>
+        <location filename="../valueChecker.py" line="145"/>
+        <source> column </source>
+        <translation> столбец </translation>
+    </message>
+    <message>
+        <location filename="../valueChecker.py" line="139"/>
+        <source> contains empty value</source>
+        <translation> не содержит значения</translation>
+    </message>
+    <message>
+        <location filename="../valueChecker.py" line="145"/>
+        <source> contains incorrect value</source>
+        <translation> содержит неправильное значение</translation>
+    </message>
+    <message>
+        <location filename="../valueChecker.py" line="152"/>
+        <source>Values errors found</source>
+        <translation>Обнаружены неправильные значения</translation>
+    </message>
+    <message>
+        <location filename="../valueChecker.py" line="145"/>
+        <source>Value error</source>
+        <translation>Ошибочное значение</translation>
+    </message>
+</context>
+<context>
+    <name>numericalDigitize_MainDialog</name>
+    <message>
+        <location filename="../ui_addFeatureGUI.py" line="286"/>
+        <source>Add numerical feature</source>
+        <translation>Добавить объект по кооридинатам</translation>
+    </message>
+    <message>
+        <location filename="../ui_addFeatureGUI.py" line="287"/>
+        <source>Coordinates are given</source>
+        <translation>Координаты точек в</translation>
+    </message>
+    <message>
+        <location filename="../ui_addFeatureGUI.py" line="289"/>
+        <source>in the CRS of the Project</source>
+        <translation>системе координат проекта</translation>
+    </message>
+    <message>
+        <location filename="../ui_addFeatureGUI.py" line="288"/>
+        <source>in the CRS of the Layer</source>
+        <translation>системе координат текущего слоя</translation>
+    </message>
+    <message>
+        <location filename="../ui_addFeatureGUI.py" line="290"/>
+        <source>other</source>
+        <translation>системе координат</translation>
+    </message>
+    <message>
+        <location filename="../ui_addFeatureGUI.py" line="291"/>
+        <source>not selected</source>
+        <translation>не выбрано</translation>
+    </message>
+    <message>
+        <location filename="../ui_addFeatureGUI.py" line="294"/>
+        <source>1</source>
+        <translation>1</translation>
+    </message>
+    <message>
+        <location filename="../ui_addFeatureGUI.py" line="296"/>
+        <source>X</source>
+        <translation>X</translation>
+    </message>
+    <message>
+        <location filename="../ui_addFeatureGUI.py" line="298"/>
+        <source>Y</source>
+        <translation>Y</translation>
+    </message>
+    <message>
+        <location filename="../ui_addFeatureGUI.py" line="299"/>
+        <source>Add part</source>
+        <translation>Добавить часть</translation>
+    </message>
+    <message>
+        <location filename="../ui_addFeatureGUI.py" line="315"/>
+        <source>...</source>
+        <translation>...</translation>
+    </message>
+    <message>
+        <location filename="../ui_addFeatureGUI.py" line="301"/>
+        <source>Remove part</source>
+        <translation>Удалить часть</translation>
+    </message>
+    <message>
+        <location filename="../ui_addFeatureGUI.py" line="303"/>
+        <source>Add polygon ring</source>
+        <translation>Добавить кольцо</translation>
+    </message>
+    <message>
+        <location filename="../ui_addFeatureGUI.py" line="305"/>
+        <source>Copy to clipboard</source>
+        <translation>Копировать координаты в буфер обмена</translation>
+    </message>
+    <message>
+        <location filename="../ui_addFeatureGUI.py" line="307"/>
+        <source>Paste from clipboard</source>
+        <translation>Вставить координаты в буфер обмена</translation>
+    </message>
+    <message>
+        <location filename="../ui_addFeatureGUI.py" line="311"/>
+        <source>Add row</source>
+        <translation>Добавить строку</translation>
+    </message>
+    <message>
+        <location filename="../ui_addFeatureGUI.py" line="313"/>
+        <source>Delete rows</source>
+        <translation>Удалить строки</translation>
+    </message>
+    <message>
+        <location filename="../ui_addFeatureGUI.py" line="309"/>
+        <source>Swap values of X and Y columns</source>
+        <translation>Обменять значения столбцов X и Y</translation>
+    </message>
+    <message>
+        <location filename="../ui_addFeatureGUI.py" line="292"/>
+        <source>Part №</source>
+        <translation>Часть №</translation>
+    </message>
+</context>
+</TS>
